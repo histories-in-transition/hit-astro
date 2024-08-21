210677067a1dcebfb03014023e3d38c078ce0d90
ivanadob <ivana.dob@gmail.com>
2024-08-21 13:39:11 +0200

    initial commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography'
+import typography from "@tailwindcss/typography";
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [typography],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brandIvory: "#FFF0EE",
+        brandRose: "#EDD4C2",
+        brandRed: "#8A4C55",
+        brandYellow: "#FFAC1D",
+      },
+    },
+  },
+  plugins: [typography],
+};
