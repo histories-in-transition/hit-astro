ee1ac25437185d3c413f7cb649be38896e4e23e6
csae8092 <p.andorfer@gmail.com>
2024-11-06 20:12:39 +0100

    theme gallery and link to work-tab into nav menu

--- src/lib/tabulator-utils.js
+++ src/lib/tabulator-utils.js
@@ -3,7 +3,7 @@ import { JSONPath } from "jsonpath-plus";
 
 export function jsonpathLookup(value, data, type, params, component) {
   const path = params.path;
-  const separator = params.separator || " >> ";
+  const separator = params.separator || ", ";
   const reverse = params.reverse || false;
   const result = JSONPath({path: path, json: value});
   if (reverse){
