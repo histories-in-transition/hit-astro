1a5a3cc110dc24a67224d59460c331fede626f8e
ivanadob <ivana.dob@gmail.com>
2024-11-06 09:56:43 +0100

    set tabulator table for works

---
+++ src/lib/tabulator-utils.js
@@ -0,0 +1,26 @@
+function mutateSelectField(value, data, type, params, component) {
+  let output = value
+    .map((item) => {
+      return `${item.value}`;
+    })
+    .join(" / ");
+  return `${output}`;
+}
+
+function mutateSingleSelectField(value, data, type, params, component) {
+  let output = value.value;
+  return `${output}`;
+}
+
+export function mutateM2mField(value, data, type, params, component) {
+  let labelField = params.labelField;
+  let names = [];
+
+  value.forEach((item) => {
+    // Add the main name
+    if (item[labelField]) {
+      names.push(item[labelField]);
+    }
+  });
+  return names.join(" ");
+}
