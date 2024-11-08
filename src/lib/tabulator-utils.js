f0460ce927fd0d32e4a6422a14458badc4b6a928
ivanadob <ivana.dob@gmail.com>
2024-11-08 09:14:19 +0100

    combine author and title for display in one column fix #23

--- src/lib/tabulator-utils.js
+++ src/lib/tabulator-utils.js
@@ -13,3 +13,19 @@ export function jsonpathLookup(value, data, type, params, component) {
   }
   
 }
+/* for several paths from one field joined in one column */
+export function jsonpathsLookup(value, data, type, params, component) {
+  const paths = params.paths;  // Assume `paths` is an array
+  const separator = params.separator || ", ";
+  const reverse = params.reverse || false;
+  
+  // Collect results from each path
+  let allResults = paths.flatMap(path => JSONPath({ path: path, json: value }));
+
+  // Optionally reverse the results and join them
+  if (reverse) {
+    allResults = allResults.reverse();
+  }
+  
+  return allResults.join(separator);
+}
