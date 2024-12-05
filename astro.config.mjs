e38b1a95587194674aba760567dafa9eca209526
ivanadob <ivana.dob@gmail.com>
2024-12-05 15:34:33 +0100

    add breadcrumbs and article nav component

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,13 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 3000
+    port: 3000,
+    open: 'hit-astro', // Automatically open at the correct base path
   },
   integrations: [tailwind(), icon()],
   site: "https://histories-in-transition.github.io",
   base: "/hit-astro", //  GitHub repo name
-  server: {
-    open: 'hit-astro', // Automatically open at the correct base path
-  },
+  
   build: {
     format: 'directory',
     cleanUrls: true
