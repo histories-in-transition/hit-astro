f65cd351d29a81547831403860d7d09bf9520608
ivanadob <ivana.dob@gmail.com>
2025-03-18 12:57:51 +0100

    upgrade to tailwindcss v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
-// https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: 'hit-astro', // Automatically open at the correct base path
-  },
-  integrations: [tailwind(), icon()],
-  site: "https://histories-in-transition.github.io",
-  base: "/hit-astro", //  GitHub repo name
-  
-  build: {
-    format: 'directory',
-    cleanUrls: true
-  }
+	server: {
+		port: 3000,
+		open: "hit-astro", // Automatically open at the correct base path
+	},
+	integrations: [icon()],
+	site: "https://histories-in-transition.github.io",
+	base: "/hit-astro", //  GitHub repo name
+
+	build: {
+		format: "directory",
+		cleanUrls: true,
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
