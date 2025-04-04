503c6fd5565c67f73858f8bdaf4a85a139b8889e
ivanadob <ivana.dob@gmail.com>
2025-04-04 13:39:23 +0200

    made map-leaf.jsx more flexible to work on all geojson also without properties.type

--- src/components/map-leaflet.jsx
+++ src/components/map-leaflet.jsx
@@ -1,16 +1,39 @@
 import { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet.markercluster";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import "leaflet/dist/leaflet.css";
 import { withBasePath } from "@/lib/withBasePath";
+//import pin from "@/icons/map-pin.png";
 
-delete L.Icon.Default.prototype._getIconUrl;
+// Create custom icon
+const pinGreen = new URL("@/icons/map-pin-green.png", import.meta.url).toString();
+const pinRed = new URL("@/icons/map-pin-red.png", import.meta.url).toString();
+const pin = new URL("@/icons/map-pin.png", import.meta.url).toString();
 
-L.Icon.Default.mergeOptions({
-	iconRetinaUrl: new URL("leaflet/dist/images/marker-icon-2x.png", import.meta.url).toString(),
-	iconUrl: new URL("leaflet/dist/images/marker-icon.png", import.meta.url).toString(),
-	shadowUrl: new URL("leaflet/dist/images/marker-shadow.png", import.meta.url).toString(),
+const originIcon = L.icon({
+	iconUrl: pinRed, // Use the same pin image
+	iconSize: [25, 25], // Adjust size as needed
+	iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+	popupAnchor: [0, -41], // Point from which the popup should open relative to the iconAnchor
 });
+
+const provenanceIcon = L.icon({
+	iconUrl: pinGreen, // Use the same pin image
+	iconSize: [25, 25], // Adjust size as needed
+	iconAnchor: [12, 41], // Point of the icon which will correspond to marker's location
+	popupAnchor: [0, -41], // Point from which the popup should open relative to the iconAnchor
+});
+const customIcon = L.icon({
+	iconUrl: pin,
+	iconSize: [25, 25], // size of the icon, adjust as needed
+	iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+	popupAnchor: [0, -41], // point from which the popup should open relative to the iconAnchor
+});
+
+// Set as default icon
+L.Marker.prototype.options.icon = customIcon;
 
 export default function LeafletMap({
 	geoJsonData,
@@ -22,6 +45,8 @@ export default function LeafletMap({
 	const mapRef = useRef(null);
 	const mapInstanceRef = useRef(null);
 	const markersRef = useRef(null);
+	const originLayerRef = useRef(null);
+	const provenanceLayerRef = useRef(null);
 
 	// Create a method that can be called from outside to update the map
 	useEffect(() => {
@@ -54,13 +79,8 @@ export default function LeafletMap({
 		if (!map) return;
 
 		// Remove existing markers cluster group if it exists
-		if (markersRef.current) {
-			map.removeLayer(markersRef.current);
-		}
-
-		// Create new marker cluster group
-		const markers = L.markerClusterGroup();
-		markersRef.current = markers;
+		originLayerRef.current.clearLayers();
+		provenanceLayerRef.current.clearLayers();
 
 		// Add markers for filtered data
 		dataToShow.features.forEach((feature) => {
@@ -73,8 +93,11 @@ export default function LeafletMap({
 				!isNaN(geometry.coordinates[0]) &&
 				!isNaN(geometry.coordinates[1])
 			) {
-				const marker = L.marker([geometry.coordinates[1], geometry.coordinates[0]]);
-
+				// Create a marker with the appropriate icon based on properties.type
+				properties.type = properties.type || "origin"; // Default to "origin" if type is not specified
+				const marker = L.marker([geometry.coordinates[1], geometry.coordinates[0]], {
+					icon: properties.type === "origin" ? originIcon : provenanceIcon,
+				});
 				if (properties) {
 					const url = withBasePath(properties.url || "");
 					const popupContent = `
@@ -86,12 +109,14 @@ export default function LeafletMap({
                     `;
 					marker.bindPopup(popupContent);
 				}
-				markers.addLayer(marker);
+				// Add marker to the appropriate layer based on properties.type
+				if (properties.type === "origin") {
+					originLayerRef.current.addLayer(marker);
+				} else if (properties.type === "provenance") {
+					provenanceLayerRef.current.addLayer(marker);
+				}
 			}
 		});
-
-		// Add markers to map
-		map.addLayer(markers);
 
 		// Fit bounds if there are features
 		if (dataToShow.features.length > 0) {
@@ -125,6 +150,71 @@ export default function LeafletMap({
 				maxZoom: 19,
 			}).addTo(map);
 
+			const originLayer = L.markerClusterGroup({
+				spiderfyOnMaxZoom: true, // Enable spiderfy when zoomed in
+				showCoverageOnHover: false,
+				zoomToBoundsOnClick: true,
+				maxClusterRadius: 50,
+				// add some offset to avoid overlapping clusters
+				iconCreateFunction: (cluster) => {
+					const count = cluster.getChildCount();
+					const offsetX = Math.random() * 10 - 5;
+					const offsetY = Math.random() * 10 - 5;
+					return L.divIcon({
+						html: `<div class="bg-brand-800 opacity-60 text-white rounded-full w-8 h-8 flex items-center justify-center">${count}</div>`,
+						className: "custom-cluster-icon-origin",
+						iconSize: [30, 30],
+						iconAnchor: [25 + offsetX, 25 + offsetY], // Apply the offset
+					});
+				},
+			});
+
+			const provenanceLayer = L.markerClusterGroup({
+				spiderfyOnMaxZoom: true, // Enable spiderfy when zoomed in
+				showCoverageOnHover: false,
+				zoomToBoundsOnClick: true,
+				maxClusterRadius: 50,
+				iconCreateFunction: (cluster) => {
+					const count = cluster.getChildCount();
+					const offsetX = Math.random() * 10 + 5;
+					const offsetY = Math.random() * 10 + 5;
+					return L.divIcon({
+						html: `<div class="bg-accent-sage-900 opacity-60 text-white rounded-full w-8 h-8 flex items-center justify-center">${count}</div>`,
+						className: "custom-cluster-icon-origin",
+						iconSize: [30, 30],
+						iconAnchor: [25 + offsetX, 25 + offsetY],
+					});
+				},
+			});
+
+			originLayerRef.current = originLayer;
+			provenanceLayerRef.current = provenanceLayer;
+
+			// Add the marker cluster groups to the map
+			map.addLayer(originLayer);
+			map.addLayer(provenanceLayer);
+
+			// Add layer control if types are present i.e. more than one layer
+
+			// Check if there are features with defined types
+			const hasOriginType = geoJsonData.features.some((f) => f.properties?.type === "origin");
+			const hasProvenanceType = geoJsonData.features.some(
+				(f) => f.properties?.type === "provenance",
+			);
+
+			// Only add layer control if we have both types
+			if (hasOriginType && hasProvenanceType) {
+				L.control
+					.layers(
+						{},
+						{
+							Entstehungsort: originLayer,
+							Provenienz: provenanceLayer,
+						},
+						{ collapsed: false },
+					)
+					.addTo(map);
+			}
 			// Store map instance for cleanup
 			mapInstanceRef.current = map;
 		}
@@ -139,9 +229,8 @@ export default function LeafletMap({
 				mapInstanceRef.current = null;
 			}
 
-			if (markersRef.current) {
-				markersRef.current = null;
-			}
+			originLayerRef.current = null;
+			provenanceLayerRef.current = null;
 		};
 	}, [geoJsonData, initialView, initialZoom, markerColor]);
 
